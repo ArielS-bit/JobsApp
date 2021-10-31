85c09a866890ad64602baf7577b8361e8a3b05d2
User <User@LAB3-9>
2021-10-31 09:58:57 +0200

    Login Changes

--- JobsApp/JobsApp/ViewModels/LoginViewModel.cs
+++ JobsApp/JobsApp/ViewModels/LoginViewModel.cs
@@ -60,7 +60,7 @@ namespace JobsApp.ViewModels
             get => emailError;
             set
             {
-                emailError = "*Invalid Email";
+                emailError = "*Invalid Emailll";
                 OnPropertyChanged("EmailError");
             }
         }
