6164b06492a29a7f7c2009e517665f1b3257ae72
User <User@LAB2-7>
2021-11-03 13:13:36 +0200

    SignUp Chnages

--- JobsApp/JobsApp/ViewModels/SignUpViewModel.cs
+++ JobsApp/JobsApp/ViewModels/SignUpViewModel.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using JobsApp.Services;
+using JobsApp.Models;
+using Xamarin.Essentials;
+using System.Linq;
+using JobsApp.ViewModels;
+using JobsApp.Views;
+
 
 namespace JobsApp.ViewModels
 {
-    class SignUpViewModel
+    class SignUpViewModel:ViewModelBase
     {
+        public event Action<Page> Push;
     }
 }
