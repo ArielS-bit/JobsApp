745cca9cc57b5083eb6b37fea19e1860bd63aaf9
Ariel Shoham <ariel.shoham400@gmail.com>
2022-01-16 10:10:12 +0200

    Fonts Awesome and Tab Desgin Fixing

--- JobsApp/JobsApp/AssemblyInfo.cs
+++ JobsApp/JobsApp/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly:ExportFont("ZenKakuGothicAntique-Regular.ttf", Alias ="RegularFont")]
+[assembly: ExportFont("ZenKakuGothicAntique-Bold.ttf", Alias = "BoldFont")]
