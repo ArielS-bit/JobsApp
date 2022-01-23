f6f64cf362a48334c7bf6c8a347ba9cbb2cee9bc
User <User@LAB3-4>
2022-01-23 08:47:32 +0200

    ForgetPass screen - half way there

--- JobsApp/JobsApp/ViewModels/ForgetPasswordViewModel.cs
+++ JobsApp/JobsApp/ViewModels/ForgetPasswordViewModel.cs
@@ -13,12 +13,93 @@ using JobsApp.ViewModels;
 
 namespace JobsApp.ViewModels
 {
-    class ForgetPasswordViewModel:ViewModelBase
+    class ForgetPasswordViewModel : ViewModelBase
     {
+        #region Properties
+        private string newPass;
+
+        public string NewPass
+        {
+            get { return newPass; }
+
+            set
+            {
+                newPass = value;
+                OnPropertyChanged("NewPass");
+            }
+
+        }
+
+        private string petName;
+
+        public string PetName
+        {
+            get { return petName; }
+
+            set
+            {
+                petName = value;
+                OnPropertyChanged("PetName");
+            }
+
+        }
+
+        private bool isVisibleCorrectEmail;
+
+        public bool IsVisibleCorrectEmail
+        {
+            get { return isVisibleCorrectEmail; }
+
+            set
+            {
+                isVisibleCorrectEmail = value;
+                OnPropertyChanged("IsVisibleCorrectEmail");
+            }
+
+        } 
         
+        private bool isVisibleCorrectPetName;
+
+        public bool IsVisibleCorrectPetName
+        {
+            get { return IsVisibleCorrectPetName; }
+
+            set
+            {
+                isVisibleCorrectPetName = value;
+                OnPropertyChanged("IsVisibleCorrectPetName");
+            }
+
+        }
+
+        #endregion
 
         public ForgetPasswordViewModel()
         {
+            NewPass = "";
+            IsVisibleCorrectPetName = false;
+            IsVisibleCorrectEmail = false;
+            PetName = "";
+
+        }
+
+        public ICommand CheckEmailCommand => new Command(CheckEmailFunc);
+        public ICommand CheckPetNameFunc => new Command(CheckPetNameFunc);
+
+
+        private bool CheckEmailFunc()
+        {
+            JobsAPIProxy proxy = JobsAPIProxy.CreateProxy();
+            //bool IsEmailExist = proxy.IsEmailExist(Email);
+
+
+        }
+        
+        private bool CheckPetNameFunc()
+        {
+            JobsAPIProxy proxy = JobsAPIProxy.CreateProxy();
+            //bool IsEmailExist = proxy.IsPetNameExist(Email);
+
 
         }
     }
