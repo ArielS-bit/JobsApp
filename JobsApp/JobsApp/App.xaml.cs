8cbcaa8fb50ed2a9b8b56623e8c7c0d81f5c2aea
Ariel Shoham <ariel.shoham400@gmail.com>
2022-06-09 11:38:55 +0300

    Update

--- JobsApp/JobsApp/App.xaml.cs
+++ JobsApp/JobsApp/App.xaml.cs
@@ -44,7 +44,7 @@ namespace JobsApp
             InitializeComponent();
             CurrentUser = null;
 
-            MainPage = new NavigationPage(new MeetOurApp());//Should be navigating to Splash Screen
+            MainPage = new NavigationPage(new UserCredentialsScreen());//Should be navigating to Splash Screen
                                                             //{
                                                             //    BarBackgroundColor = Color.Transparent
                                                             //};
